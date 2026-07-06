40bc74301609829f8ff19f7658df1d1ff70b6878
Pasquale Giorgio <pasquale.giorgio@pl.abb.com>
2026-07-06 18:03:06 +0200

    refactor(app): use RouterLink and add smooth scrolling

--- apps/angular/21-anchor-navigation/src/app/nav-button.component.ts
+++ apps/angular/21-anchor-navigation/src/app/nav-button.component.ts
@@ -1,17 +1,28 @@
 /* eslint-disable @angular-eslint/component-selector */
 import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'nav-button',
   template: `
-    <a [href]="href()">
+    <a [routerLink]="linkCommand.path" [fragment]="linkCommand.fragment">
       <ng-content />
     </a>
   `,
   host: {
     class: 'block w-fit border border-red-500 rounded-md p-4 m-2',
   },
+  imports: [RouterLink],
 })
 export class NavButtonComponent {
-  href = input('');
+  href = input<string>('');
+
+  get linkCommand(): { path: string; fragment?: string } {
+    return {
+      path: this.href().startsWith('/') ? this.href() : '',
+      fragment: this.href().startsWith('#')
+        ? this.href().replace('#', '')
+        : undefined,
+    };
+  }
 }
