435d769214f48b2e8ede46f7758863962909fd1c
Pasquale Giorgio <pasquale.giorgio@pl.abb.com>
2026-07-23 15:53:31 +0200

    fix(app): fix typing of the pipe's transform function

--- apps/angular/10-utility-wrapper-pipe/src/app/person-utils.pipe.ts
+++ apps/angular/10-utility-wrapper-pipe/src/app/person-utils.pipe.ts
@@ -1,16 +1,36 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { PersonUtils } from './person.utils';
 
-type UtilsFunctions = {
+type UtilsMap = {
   showName: typeof PersonUtils.showName;
   isAllowed: typeof PersonUtils.isAllowed;
 };
+type FunctionArgs<T extends keyof UtilsMap> = Parameters<UtilsMap[T]>;
+type ShowNameArgs = FunctionArgs<'showName'>;
+type isAllowedArgs = FunctionArgs<'isAllowed'>;
+
+type FunctionReturn<T extends keyof UtilsMap> = ReturnType<UtilsMap[T]>;
+type ShowNameReturn = FunctionReturn<'showName'>;
+type isAllowedReturn = FunctionReturn<'isAllowed'>;
 
 @Pipe({
   name: 'personUtils',
 })
 export class PersonUtilsPipe implements PipeTransform {
-  transform(functionName: keyof UtilsFunctions, ...args: unknown[]): string {
+  transform(
+    functionName: 'showName',
+    arg1: ShowNameArgs[0],
+    arg2: ShowNameArgs[1],
+  ): ShowNameReturn;
+
+  transform(
+    functionName: 'isAllowed',
+    arg1: isAllowedArgs[0],
+    arg2: isAllowedArgs[1],
+    arg3: isAllowedArgs[2],
+  ): isAllowedReturn;
+
+  transform(functionName: keyof UtilsMap, ...args: unknown[]): string {
     if (functionName === 'showName')
       return PersonUtils.showName(args[0] as string, args[1] as number);
 
