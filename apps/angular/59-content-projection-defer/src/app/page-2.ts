175f45142e90933ca3482b222712db383b689c39
Pasquale Giorgio <pasquale.giorgio@pl.abb.com>
2026-07-24 17:16:21 +0200

    fix(app): make api request start after section expanded

--- apps/angular/59-content-projection-defer/src/app/page-2.ts
+++ apps/angular/59-content-projection-defer/src/app/page-2.ts
@@ -1,5 +1,5 @@
 import { httpResource } from '@angular/common/http';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { ExpandableCard } from './expandable-card';
 
 interface Post {
@@ -13,17 +13,17 @@ interface Post {
   selector: 'app-page-2',
   template: `
     page2
-    <app-expandable-card>
+    <app-expandable-card (isExpandedChanged)="isExpanded.set($event)">
       <div title>Load Post</div>
       <div>
-        @if (postResource.isLoading()) {
-          Loading...
-        } @else if (postResource.status() === 'error') {
-          Error...
-        } @else {
+        @defer {
           @for (post of postResource.value(); track post.id) {
             <div>{{ post.title }}</div>
           }
+        } @loading {
+          Loading...
+        } @error {
+          Error...
         }
       </div>
     </app-expandable-card>
@@ -32,7 +32,10 @@ interface Post {
   imports: [ExpandableCard],
 })
 export class Page2 {
-  public postResource = httpResource<Post[]>(
-    () => 'https://jsonplaceholder.typicode.com/posts',
-  );
+  public isExpanded = signal<boolean>(false);
+
+  public postResource = httpResource<Post[]>(() => {
+    if (this.isExpanded()) return 'https://jsonplaceholder.typicode.com/posts';
+    return undefined;
+  });
 }
